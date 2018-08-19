5be26c0dd8fc0915ce02cff1ca897373398d06bf
Personal <personal@macbook-pro.lan>
2018-08-19 16:05:24 -0400

    Add client side testing framework and API route to server

--- server/index.js
+++ server/index.js
@@ -4,9 +4,10 @@ const { Nuxt, Builder } = require('nuxt')
 const app = express()
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
-
+const apiRoutes = require('./api');
 app.set('port', port)
 
+app.use('/api', apiRoutes);
 // Import and Set Nuxt.js options
 let config = require('../nuxt.config.js')
 config.dev = !(process.env.NODE_ENV === 'production')
